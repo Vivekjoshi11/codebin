9be074c023c2d72920f8e80c933c631003ae5311
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-09-06 18:17:04 +0530

    reactive form and template driven form. also include notfound page route. when nothing will there or there is no page there so this page will shown

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { AboutComponent } from './component/about/about.component';
 
 export const routes: Routes = [
     {path: 'login', component:LoginComponent},
     {path: 'signup', component:SignupComponent},
+    {path: 'about', component:AboutComponent},
+    {path:'', redirectTo:"/login",pathMatch:"full"},
+
+
+    {path:'**', component:NotFoundComponent}  // this will check if above routes are not there than it will be on notfound page 
+    // and it should be last on the routers because it chech abouves routes
 ];
