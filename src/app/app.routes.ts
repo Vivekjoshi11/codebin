004382c5887a2b206a43dcd3ad4b80bc5521e7dd
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-09-06 15:04:39 +0530

    login form and validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: 'signup', component:SignupComponent},
+];
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  
+
+  email= new FormControl("",[
+    Validators.required,
+    Validators.email 
+  ])
+
+  password = new FormControl("",
+    [Validators.required,
+      Validators.minLength(6)
+    ]
+  )
+
+  loginForm = new FormGroup([
+     this.email,
+     this.password
+  ])
+  login(){
+    console.log(this.loginForm.value)
+  }
+  reset() {
+    // throw new Error('Method not implemented.');
+    this.loginForm.reset()
+    }
 }
