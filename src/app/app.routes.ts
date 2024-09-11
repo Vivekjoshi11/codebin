d0ce4343e071b026a7902d5665678b1eb770bde7
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-09-11 22:07:06 +0530

    authguard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { SignupComponent } from './component/signup/signup.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 import { AboutComponent } from './component/about/about.component';
 import { CreateBinComponent } from './component/create-bin/create-bin.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: 'login', component:LoginComponent},
     {path: 'signup', component:SignupComponent},
     {path: 'about', component:AboutComponent},
-    {path: 'create', component:CreateBinComponent},
+    {path: 'create', component:CreateBinComponent, canActivate:[authGuard]},
     {path: '', loadComponent:()=> import('./component/about/about.component').then(mod => mod.AboutComponent) },  // lazy loading
     {path:'', redirectTo:"/login",pathMatch:"full"},
 
